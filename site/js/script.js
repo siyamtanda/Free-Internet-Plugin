7142adf9874935ae2a2eca75a660c27ff69a9d36
Guy Torbet <guy@torbet.co>
2021-05-11 16:42:13 +0100

    updated landing

--- site/js/script.js
+++ site/js/script.js
@@ -2,17 +2,30 @@ var arrow = document.querySelector('#arrow');
 
 arrow.addEventListener('click', () => {
     document.getElementById('bottom').scrollIntoView({behavior: "smooth", block: "start"})
+    subPage()
     
 })
 
-var i = 0;
-var txt = 'Free Internet Plugin'
-var speed = 75; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+function type(id, text, speed) {
+  var i = 0;
+  var txt = text;
+  document.getElementById(id).innerHTML = ""
+
+  typeWriter()
+  
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById(id).innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
   }
+}
+
+function subPage() {
+    type("whyTitle", "Why?", 200)
+    type("whoTitle", "Who?", 200)
+    window.onscroll = null;
+  
 }
